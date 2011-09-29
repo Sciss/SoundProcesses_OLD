4d9752b8ac3e4cec7094e2c6d4bcced659c7034f
Sciss <contact@sciss.de>
2011-09-29 23:48:29 +0200

    expanding SynthGraph now in every case, even when cache'd, due to includeParam problem with output bus. anyway, a future version will evaluate graph { } straight away and may save side effects in the proc structure?

--- src/main/scala/de/sciss/synth/proc/ProcWorld.scala
+++ src/main/scala/de/sciss/synth/proc/ProcWorld.scala
@@ -29,8 +29,8 @@
 package de.sciss.synth.proc
 
 import collection.immutable.{ IndexedSeq => IIdxSeq, Set => ISet }
-import de.sciss.synth.{addAfter, addBefore, Group, Server, SynthDef, SynthGraph}
 import sys.error
+import de.sciss.synth.{UGenGraph, addAfter, addBefore, Group, Server, SynthDef, SynthGraph}
 
 object ProcWorld {
 //   case class ProcsRemoved( procs: Proc* )
@@ -46,7 +46,8 @@ class ProcWorld extends TxnModel[ ProcWorld.Update ] {
    import ProcWorld._
 
    private type Topo = Topology[ Proc, ProcEdge ] 
-   val synthGraphs = Ref( Map.empty[ SynthGraph, RichSynthDef ])
+//   val synthGraphs = Ref( Map.empty[ SynthGraph, RichSynthDef ])
+   val ugenGraphs = Ref( Map.empty[ UGenGraph, RichSynthDef ])
    private val topologyRef = Ref[ Topo ]( Topology.empty )
 
    protected def fullUpdate( implicit tx: ProcTxn ) = Update( topologyRef().vertices.toSet, Set.empty )
@@ -213,12 +214,22 @@ object ProcDemiurg extends TxnModel[ ProcDemiurgUpdate ] { // ( val server: Serv
 
    def getSynthDef( server: Server, graph: SynthGraph )( implicit tx: ProcTxn ) : RichSynthDef = syn.synchronized {
       val w    = worlds( server )
-      w.synthGraphs().get( graph ).getOrElse {
-//println( "Graph: " + graph )
+//      w.synthGraphs().get( graph ).getOrElse {
+//         val name = "proc" + nextDefID
+//         val rd   = RichSynthDef( server, SynthDef( name, graph.expand ))
+//         w.synthGraphs.transform( _ + (graph -> rd) )
+//         rd
+//      }
+
+      // XXX note: unfortunately we have sideeffects in the expansion, such as
+      // includeParam for ProcAudioOutput ... And anyways, we might allow for
+      // indeterminate GE.Lazies, thus we need to check for UGenGraph equality,
+      // not SynthGraph equality
+      val u = graph.expand
+      w.ugenGraphs().get( u ).getOrElse {
          val name = "proc" + nextDefID
-         val rd   = RichSynthDef( server, SynthDef( name, graph.expand ))
-         w.synthGraphs.transform( _ + (graph -> rd) )
-//         tx.add( server, rd.synthDef.recvMsg )
+         val rd   = RichSynthDef( server, SynthDef( name, u ))
+         w.ugenGraphs.transform( _ + (u -> rd) )
          rd
       }
    }
